299e7edc8bea47fe2c1680fb27f04eeceef4046f
sjkyomg <sjkyomg@easyshop.co.kr>
2024-11-04 11:18:17 +0900

    test commit

---
+++ src/main/java/com/shop/oyyproject/OyyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shop.oyyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OyyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OyyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/oyyproject/OyyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.oyyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OyyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
